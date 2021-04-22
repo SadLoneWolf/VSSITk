d73ced2915bb32ffd0150e3cfb297e0f9631579c
SadLoneWolf <djordjeandjic99@gmail.com>
2021-04-22 17:19:01 +0200

    Finish order page

---
+++ Lab4/JavaScript/hide_order.js
@@ -0,0 +1,39 @@
+function toggleCoffe(){
+    var coffe = document.getElementById('coffe_list')
+    console.log(coffe)
+    if(coffe.style.display == "none"){
+        coffe.style.display = "block"
+    }else{
+        coffe.style.display = "none"
+    }
+}
+
+function toggleSalad(){
+    var salad = document.getElementById("salad_list")
+    console.log(salad)
+    if(salad.style.display == "none"){
+        salad.style.display = "block"
+    }else{
+        salad.style.display = "none"
+    }
+}
+
+function toggleJuice(){
+    var soda= document.getElementById("soda_list")
+    console.log(soda)
+    if(soda.style.display == "none"){
+        soda.style.display = "block"
+    }else{
+        soda.style.display = "none"
+    }
+}
+
+function toggleCookies(){
+    var cookie = document.getElementById("cookie_list")
+    console.log(cookie)
+    if(cookie.style.display == "none"){
+        cookie.style.display = "block"
+    }else{
+        cookie.style.display = "none"
+    }
+}
