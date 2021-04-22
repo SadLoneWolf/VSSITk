aac11f10d47dae79b011cd89fcc6f8caf577a3f0
SadLoneWolf <djordjeandjic99@gmail.com>
2021-04-22 12:50:06 +0200

    DOM problems

---
+++ Lab4/JavaScript/index_animation.js
@@ -0,0 +1,10 @@
+function mobileMenu(){
+
+    var menu = document.getElementById('header_menu')
+    console.log(document.getElementById('header_menu'))
+    if(menu.style.display == "block"){
+        menu.style.display == "none"
+    }else{
+        menu.style.display == "block"
+    }
+}
